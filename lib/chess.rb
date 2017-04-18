62066254e55fe9105cc71f88363b2bd5d220f8ea
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-04-18 20:27:19 +0300

    Add spec for Game class

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,5 +1,19 @@
 require_relative 'board.rb'
 
+class Game
+
+	attr_accessor :board
+
+	white_player = { rook: "\u2656", knight: "\u2658", bishop: "\u2657", pawn: "\u2659", king: "\u2654", queen: "\u2655" }
+	black_player = { rook: "\u265c", knight: "\u265E", bishop: "\u265D", pawn: "\u265F", king: "\u265A", queen: "\u265B" }
+
+	def initialize
+		@board = Board.new
+	end
+
+
+end
+
 board = Board.new
 
 board.show_board
---
+++ spec/chess_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'chess.rb'
+
+describe Game do
+
+	let(:game) { Game.new }
+	
+	context '#initialize' do
+		it 'creates a chess board' do
+			expect(game.board).to 
+		end
+	end
+
+
+end
