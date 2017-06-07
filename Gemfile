f841f30e89904efa4392b53997f5c59724b717f7
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-06-07 19:52:00 +0300

    Refactor queen and specify ruby version in Gemfile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "http://rubygems.org"
 
+ruby '2.3.3'
+
 gem 'rspec', '3.6.0'
 
 gem 'rubocop', '~> 0.49.1', require: false
--- lib/queen.rb
+++ lib/queen.rb
@@ -23,36 +23,36 @@ class Queen
 
 		right = ver + 1
     if right < 8
-      while right <= 8 && not_ally?(board,hor,right)
-        @possible_moves.push [hor,right]
-        break if enemy?(board,hor,right)
+      while right <= 8 && not_ally?(board, hor, right)
+        @possible_moves.push [hor, right]
+        break if enemy?(board, hor, right)
         right += 1
       end
     end
 
 		left = ver - 1
     if left > 0
-      while left > 0 && not_ally?(board,hor,left)
-        @possible_moves.push [hor,left]
-        break if enemy?(board,hor,left)
+      while left > 0 && not_ally?(board, hor, left)
+        @possible_moves.push [hor, left]
+        break if enemy?(board, hor, left)
         left -= 1
       end
     end
 
 		up = hor - 1
     if up > 0
-      while up > 0 && not_ally?(board,up,ver)
-        @possible_moves.push [up,ver]
-        break if enemy?(board,up,ver)
+      while up > 0 && not_ally?(board, up, ver)
+        @possible_moves.push [up, ver]
+        break if enemy?(board, up, ver)
         up -= 1
       end
     end
 
 		down = hor + 1
     if down <= 8
-      while down <= 8 && not_ally?(board,down,ver)
-        @possible_moves.push [down,ver]
-        break if enemy?(board,down,ver)
+      while down <= 8 && not_ally?(board, down, ver)
+        @possible_moves.push [down, ver]
+        break if enemy?(board, down, ver)
         down += 1
       end
     end
@@ -60,13 +60,13 @@ class Queen
 		east = ver + 1
     north = hor - 1
     if east <= 8 && north > 0
-      while east <= 8 && north > 0 && not_ally?(board,north,east)
-        if enemy?(board,north,east)
-          @possible_moves.push [north,east]
+      while east <= 8 && north > 0 && not_ally?(board, north, east)
+        if enemy?(board, north, east)
+          @possible_moves.push [north, east]
           break
         end
-        break if ally?(board,north,east)
-        @possible_moves.push [north,east]
+        break if ally?(board, north, east)
+        @possible_moves.push [north, east]
         east += 1
         north -= 1
       end
@@ -74,13 +74,13 @@ class Queen
 		west = ver - 1
     north = hor - 1
     if west > 0 && north > 0
-      while west > 0 && north > 0 && not_ally?(board,north,west)
-        if enemy?(board,north,west)
-          @possible_moves.push [north,west]
+      while west > 0 && north > 0 && not_ally?(board, north, west)
+        if enemy?(board, north, west)
+          @possible_moves.push [north, west]
           break
         end
-        break if ally?(board,north,west)
-        @possible_moves.push [north,west]
+        break if ally?(board, north, west)
+        @possible_moves.push [north, west]
         west -= 1
         north -= 1
       end
@@ -88,13 +88,13 @@ class Queen
 		south = hor + 1
     east = ver + 1
     if south <= 8 && east <= 8
-      while south <= 8 && east <= 8 && not_ally?(board,south,east)
-        if enemy?(board,south,east)
-          @possible_moves.push [south,east]
+      while south <= 8 && east <= 8 && not_ally?(board, south, east)
+        if enemy?(board, south, east)
+          @possible_moves.push [south, east]
           break
         end
-        break if ally?(board,south,east)
-        @possible_moves.push [south,east]
+        break if ally?(board, south, east)
+        @possible_moves.push [south, east]
         south += 1
         east += 1
       end
@@ -102,13 +102,13 @@ class Queen
 		south = hor + 1
     west = ver - 1
     if south <= 8 && west > 0
-      while south <= 8 && west > 0 && not_ally?(board,south,west)
-        if enemy?(board,south,west)
-          @possible_moves.push [south,west]
+      while south <= 8 && west > 0 && not_ally?(board, south, west)
+        if enemy?(board, south, west)
+          @possible_moves.push [south, west]
           break
         end
-        break if ally?(board,south,west)
-        @possible_moves.push [south,west]
+        break if ally?(board,south, west)
+        @possible_moves.push [south, west]
         south += 1
         west -= 1
       end
